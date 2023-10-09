ef21be71ab55220ee191fa16334afff441cb7236
alirfanyasin <risayunita21@gmail.com>
2023-10-09 08:58:32 +0700

    import

---
+++ OOP/16_Import/Main.java
@@ -0,0 +1,13 @@
+// package OOP.16_Import;
+
+// import nama_package
+
+// untuk mengimport semua package
+// import nama_package*
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
