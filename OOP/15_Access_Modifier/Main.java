111b1370c6c12892ff2432a3eb8961ddf1e9af54
alirfanyasin <risayunita21@gmail.com>
2023-10-09 08:49:13 +0700

    access modifier

---
+++ OOP/15_Access_Modifier/Main.java
@@ -0,0 +1,17 @@
+// package OOP.15_Access_Modifier;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Access Modifier
+    /*===================================================================
+     *   Modifier     ::  Class  ::  Package  ::  Subclass  ::  World
+     *===================================================================
+     *   Public       ::   Yes   ::    Yes    ::    Yes     ::   Yes
+     *   Protected    ::   Yes   ::    Yes    ::    Yes     ::   No
+     *   Default      ::   Yes   ::    Yes    ::    No      ::   No
+     *   Private      ::   Yes   ::    No     ::    No      ::   No
+     */
+
+  }
+}
