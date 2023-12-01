a2abed774e7667b7bd1130e65239636cae25328f
alirfanyasin <risayunita21@gmail.com>
2023-12-01 18:21:33 +0700

    to string method

---
+++ OOP/ToStringMethod/Product.java
@@ -0,0 +1,16 @@
+package OOP.ToStringMethod;
+
+public class Product {
+
+  public String name;
+  public int price;
+
+  public Product(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public String toString() {
+    return "Product name : " + name + ", Price : " + price;
+  }
+}
