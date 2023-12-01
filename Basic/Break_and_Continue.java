e6fa69bb4cbca68eae1189c5edf39d2beba7c202
alirfanyasin <risayunita21@gmail.com>
2023-12-01 16:02:38 +0700

    braek and continue

--- Basic/Break_and_Continue.java
+++ Basic/Break_and_Continue.java
@@ -1,18 +1,20 @@
 public class Break_and_Continue {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            if(i == 3) {
-                break;
-            }
-            System.out.println("Loop to " + i);
-        }
 
-        for (int a = 0; a < 5; a++){
-            if(a == 2) {
-                continue;
-            }
-            System.out.println(a);
-        }
+  public static void main(String[] args) {
+    // Break berfungsi untuk menghentikan program dalam kondisi tertentu
+    for (int i = 0; i < 5; i++) {
+      if (i == 3) {
+        break;
+      }
+      System.out.println("Loop to " + i);
     }
 
+    // Continue berfungsi untuk melangkahi (skip) program dalam kondisi tertentu
+    for (int a = 0; a < 5; a++) {
+      if (a == 2) {
+        continue;
+      }
+      System.out.println(a);
+    }
+  }
 }
