9d7a4cf1743d89c3aa96f349e0bbef79d9ce8a80
alirfanyasin <risayunita21@gmail.com>
2023-12-01 15:58:07 +0700

    ternary operator

---
+++ Basic/Ternary.java
@@ -0,0 +1,8 @@
+public class Ternary {
+
+  public static void main(String[] args) {
+    int nilai = 50;
+    String ucapan = nilai >= 60 ? "Selamat" : "Anda Gagal";
+    System.out.println(ucapan);
+  }
+}
